99398a8e2c0ca4c5a10918f6cd113bd828070d60
renil <renilgarala360004@gmail.com>
2025-06-05 15:08:28 +0530

    feat: create apikeySchema

---
+++ src/models/Api_keys.models.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const apikeySchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  key: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  expiresAt: {
+    type: Date,
+    default: null,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  lastUsedAt: {
+    type: Date,
+    default: null,
+  },
+});
+
+
+const ApiKey = mongoose.model("Apikey", apikeySchema);
+
+export default ApiKey;
