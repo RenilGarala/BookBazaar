06f69d41a056e548a878013527a123acc5cb6a24
renil <renilgarala360004@gmail.com>
2025-06-13 23:20:04 +0530

    feat: create review schema

---
+++ src/models/Reviews.models.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    book: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Book",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    comment: {
+      type: String,
+      trim: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Review = mongoose.model("Review", reviewSchema);
